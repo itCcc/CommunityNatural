9974650274d23df3158508d54d089f753fbf51b8
itCcc <c5153520@126.com>
2019-11-02 16:45:54 +0800

    完善细节

--- src/main/java/com/majiang/community/provider/GithubProvider.java
+++ src/main/java/com/majiang/community/provider/GithubProvider.java
@@ -3,55 +3,48 @@ package com.majiang.community.provider;
 import com.alibaba.fastjson.JSON;
 import com.majiang.community.dto.AccessTokenDTO;
 import com.majiang.community.dto.GithubUser;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
+/**
+ * Created by codedrinker on 2019/4/24.
+ */
 @Component
+@Slf4j
 public class GithubProvider {
-
-    @Value("${githubProvider.post.uri}")
-    private String postUri;
-
-    @Value("${githubProvider.get.uri}")
-    private String getUri;
-
-
-    public String getAccessToken(AccessTokenDTO accessTokenDTO){
-        final MediaType mediaType
-                = MediaType.get("application/json; charset=utf-8");
-
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
-            RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
-            Request request = new Request.Builder()
-                    .url(postUri)
-                    .post(body)
-                    .build();
-            try (Response response = client.newCall(request).execute()) {
-                String string = response.body().string();
-                String[] tokenstr = string.split("&");
-                String token = tokenstr[0].split("=")[1];
-                return token;
-        } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+            log.error("getAccessToken error,{}", accessTokenDTO, e);
+        }
+        return null;
     }
 
-    public GithubUser getUser(String accessToken){
+
+    public GithubUser getUser(String accessToken) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url(getUri+"="+accessToken)
+                .url("https://api.github.com/user?access_token=" + accessToken)
                 .build();
         try {
             Response response = client.newCall(request).execute();
             String string = response.body().string();
             GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
             return githubUser;
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            log.error("getUser error,{}", accessToken, e);
         }
         return null;
     }
